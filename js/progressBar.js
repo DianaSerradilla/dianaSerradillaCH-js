84314bdd7ab93cc8a7d59318605857b7affd359e
DianaSerradilla <serradilladiana3@gmail.com>
2022-05-18 09:54:51 -0300

    Arreglo ruta complementario

--- js/progressBar.js
+++ js/progressBar.js
@@ -20,7 +20,7 @@ function progressbar(idOne, idTwo, cantidad, value) {
 }
 
 
-progressbar("#circular-progressA", "#value-containerA", 25, 0);
-progressbar("#circular-progressB", "#value-containerB", 50, 0);
+progressbar("#circular-progressA", "#value-containerA", 50, 0);
+progressbar("#circular-progressB", "#value-containerB", 25, 0);
 progressbar("#circular-progressC", "#value-containerC", 0, -1);
 progressbar("#circular-progressD", "#value-containerD", 0, -1);
