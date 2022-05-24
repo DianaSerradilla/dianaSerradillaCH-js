403927b953d9ca0dd14f9494a70ec50a38e26d5c
Diana Serradilla <dianas@itcsoluciones.com>
2022-05-24 09:28:36 -0300

    Desafio dos terminado

--- js/progressBar.js
+++ js/progressBar.js
@@ -21,6 +21,6 @@ function progressbar(idOne, idTwo, cantidad, value) {
 
 
 progressbar("#circular-progressA", "#value-containerA", 75, 0);
-progressbar("#circular-progressB", "#value-containerB", 25, 0);
+progressbar("#circular-progressB", "#value-containerB", 50, 0);
 progressbar("#circular-progressC", "#value-containerC", 50, 0);
 progressbar("#circular-progressD", "#value-containerD", 0, -1);
